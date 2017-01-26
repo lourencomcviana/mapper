1152b120ee7dc5b7fa7571d87941bb9b5a3a3d36
lourenço viana <lourenco.m.c.viana@gmail.com>
2017-01-26 15:42:10 -0300

    adição de exemplos

---
+++ app/examples/moduleExample.ts
@@ -0,0 +1,12 @@
+//declare let mapper: String;
+
+class Module{
+    test :String= 'Ler isso aqui: https://blogs.msdn.microsoft.com/jtarquino/2016/01/24/debugging-typescript-in-visual-studio-code-and-chrome/';
+
+};
+
+var example = new Module();
+
+
+export {example};
+
--- app/index.ts
+++ app/index.ts
@@ -1,3 +1,8 @@
+//namespace
+/// <reference path="examples/nameSpaceExample.ts" />
+//modulo
+import { example } from "./examples/moduleExample";
 import { mapper } from "./mapper";
+
 
 console.log(mapper.test);
